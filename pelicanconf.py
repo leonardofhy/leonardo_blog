3491cd13df6cf00bb39c9d4123777b26b118badd
Leonardo298 <leonardofoohy@gmail.com>
2025-05-04 15:44:19 +0800

    feat: Apply Flex theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,6 +8,8 @@ TIMEZONE = 'Asia/Taipei'
 
 DEFAULT_LANG = 'en'
 
+THEME = 'pelican-themes/Flex'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
